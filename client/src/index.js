10012c73e19db9a152e0da164b8ca5b93c021cd5
Adam Hayes <adha9240@colorado.edu>
2022-03-29 14:34:46 -0600

    creating data_collector pseudo-app

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import {requestListing} from './services/makeRequests';
-
-const schedule = require('node-schedule');
-const job = schedule.scheduleJob('*/1 * * * *', function() {
-  requestListing();
-});
 
 ReactDOM.render(<App />, document.getElementById("root"));

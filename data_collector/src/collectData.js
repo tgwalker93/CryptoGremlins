131f25957e0da17828cdcc64d40fa8da6e53912e
Adam Hayes <adha9240@colorado.edu>
2022-04-02 13:20:16 -0600

    fixing data collector api requests

--- data_collector/src/collectData.js
+++ data_collector/src/collectData.js
@@ -2,8 +2,17 @@
 // import {requestListing} from './services/makeRequests';
 
 var request = require('./services/makeRequests');
+var apikey = {
+    key: '461f750d-2657-4821-89f9-4b0659d0f35a'
+  }
 
-request;
+request('GET', 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apikey.key)
+.then((req) => {
+    var x = JSON.parse(req.target.responseText);
+    console.log(x.data);
+}).catch(err => {
+    console.log(err);
+})
 
 // const schedule = require('node-schedule');
 // const job = schedule.scheduleJob('*/1 * * * *', function() {

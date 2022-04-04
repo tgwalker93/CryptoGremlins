3efa725974752820ff2371e1133a9858cfee90d4
Tyler Walker <tgwalker93@gmail.com>
2022-04-04 12:32:30 -0600

    cant figure out how to test react with jest, but atleaast jest is working

--- client/src/pages/landing-page/landing-page.test.js
+++ client/src/pages/landing-page/landing-page.test.js
@@ -1,4 +1,8 @@
-const landingpage = require('./landing-page');
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+// import LandingPage from './landing-page';
+
+//const landingpage = require('./landing-page');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(1+2).toBe(3);

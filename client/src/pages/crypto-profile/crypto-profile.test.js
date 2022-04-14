d5e7a2c3272e2dee8c2305b2c70b88474c0efcbc
Adam Hayes <adha9240@colorado.edu>
2022-04-14 15:56:28 -0600

    fixed crypto profile test

--- client/src/pages/crypto-profile/crypto-profile.test.js
+++ client/src/pages/crypto-profile/crypto-profile.test.js
@@ -2,18 +2,19 @@
  * @jest-environment jsdom
  */
 
- import React from 'react';
- import ReactDOM from "react-dom";
- import CryptoProfilePage from './crypto-profile';
- import { Router } from 'react-router-dom';
- 
- 
-//  test('If Crypto Profile Page is rendered', () => {
-//    const page  = ReactDOM.render(<Router><CryptoProfilePage /></Router>, 
-//      document.getElementById('root') || document.createElement('div'));
-//  })
- 
-//Above test is not working, so this is just a dummy test
-test('adds 1 + 2 to equal 3', () => {
-    expect(1+2).toBe(3);
-  });
+import React from 'react';
+import ReactDOM from "react-dom";
+import CryptoProfilePage from './crypto-profile';
+import { BrowserRouter as Router} from "react-router-dom";
+
+
+test('If Crypto Profile Page is rendered', () => {
+  const page  = ReactDOM.render(
+    <Router>
+      <CryptoProfilePage params={ {id: 'btc'} }/>
+    </Router>, 
+    document.getElementById('root') || document.createElement('div'));
+
+    expect(page).toEqual(null);
+});
+

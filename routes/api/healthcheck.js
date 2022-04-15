d040ae452293e8f1b2597d645e66a46019925f95
adamhaze <adha9240@colorado.edu>
2022-04-14 19:08:10 -0600

    healthcheck

---
+++ routes/api/healthcheck.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const router = express.Router({});
+
+
+router.get('/healthcheck', async (_req, res) => {
+    const healthcheck = {
+        uptime: process.uptime(),
+        message: 'Ok',
+        date: new Date()
+    };
+    try {
+		res.send(healthcheck);
+	} catch (e) {
+		healthcheck.message = e;
+		res.status(503).send();
+	}
+});
+
+module.exports = router;

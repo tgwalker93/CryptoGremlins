520f2955139ecfdfb734a7ddc3a2c4b89089c36a
adamhaze <adha9240@colorado.edu>
2022-04-14 20:46:14 -0600

    improving unit test coverage

--- client/src/pages/crypto-list/crypto-list.test.js
+++ client/src/pages/crypto-list/crypto-list.test.js
@@ -5,10 +5,17 @@
  import React from 'react';
  import ReactDOM from "react-dom";
  import CryptoListPage from './crypto-list';
+ import {render} from '@testing-library/react'
  
  
  test('If Crypto List Page is rendered', () => {
    const page  = ReactDOM.render(<CryptoListPage />, 
      document.getElementById('root') || document.createElement('div'));
  })
+
+//  test('If Crypto Profile Page is rendered', () => {
+//   const page  = render(<CryptoListPage />);
+
+//     expect(page.baseElement.textContent).toEqual("View Crypto Projects");
+// });
  
--- client/src/pages/crypto-profile/crypto-profile.test.js
+++ client/src/pages/crypto-profile/crypto-profile.test.js
@@ -6,15 +6,22 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import CryptoProfilePage from './crypto-profile';
 import { BrowserRouter as Router} from "react-router-dom";
+import {render} from '@testing-library/react'
+
+
+// test('If Crypto Profile Page is rendered', () => {
+//   const page  = ReactDOM.render(
+//     <Router>
+//       <CryptoProfilePage params={ {id: 'btc'} }/>
+//     </Router>, 
+//     document.getElementById('root') || document.createElement('div'));
+
+//     expect(page).toEqual(null);
+// });
 
 
 test('If Crypto Profile Page is rendered', () => {
-  const page  = ReactDOM.render(
-    <Router>
-      <CryptoProfilePage params={ {id: 'btc'} }/>
-    </Router>, 
-    document.getElementById('root') || document.createElement('div'));
+  const page  = render(<Router><CryptoProfilePage params={ {id: 'btc'} }/></Router>);
 
-    expect(page).toEqual(null);
+    expect(page.baseElement.textContent).toEqual("Back to Crypto Listings Page?  No Results to Display ");
 });
-

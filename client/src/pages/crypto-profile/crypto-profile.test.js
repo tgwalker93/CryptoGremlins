aca66a3fa5e5859fc41b8145c0dd559d85eb6484
Tyler Walker <tgwalker93@gmail.com>
2022-04-15 22:16:10 -0600

    adding testing-library/react

--- client/src/pages/crypto-profile/crypto-profile.test.js
+++ client/src/pages/crypto-profile/crypto-profile.test.js
@@ -9,19 +9,19 @@ import { BrowserRouter as Router} from "react-router-dom";
 import {render} from '@testing-library/react'
 
 
-// test('If Crypto Profile Page is rendered', () => {
-//   const page  = ReactDOM.render(
-//     <Router>
-//       <CryptoProfilePage params={ {id: 'btc'} }/>
-//     </Router>, 
-//     document.getElementById('root') || document.createElement('div'));
-
-//     expect(page).toEqual(null);
-// });
-
-
 test('If Crypto Profile Page is rendered', () => {
-  const page  = render(<Router><CryptoProfilePage params={ {id: 'btc'} }/></Router>);
+  const page  = ReactDOM.render(
+    <Router>
+      <CryptoProfilePage params={ {id: 'btc'} }/>
+    </Router>, 
+    document.getElementById('root') || document.createElement('div'));
 
-    expect(page.baseElement.textContent).toEqual("Back to Crypto Listings Page?  No Results to Display ");
+    expect(page).toEqual(null);
 });
+
+
+// test('If Crypto Profile Page is rendered', () => {
+//   const page  = render(<Router><CryptoProfilePage params={ {id: 'btc'} }/></Router>);
+
+//     expect(page.baseElement.textContent).toEqual("Back to Crypto Listings Page?  No Results to Display ");
+// });

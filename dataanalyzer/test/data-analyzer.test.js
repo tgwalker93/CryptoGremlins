554fa9e35a4a924504ca535889f9ff82a472b13e
Tyler Walker <tgwalker93@gmail.com>
2022-04-21 00:19:46 -0600

    trying to fix jest

--- dataanalyzer/test/data-analyzer.test.js
+++ dataanalyzer/test/data-analyzer.test.js
@@ -20,7 +20,7 @@ const databaseName = 'test';
   // (3) try to save comment with non string text -- make sure this comment does not show up
       // gonna need to integrate findComments.js first
 
-jest.setTimeout(30000);
+jest.setTimeout(3000000);
 
 beforeAll(async () => {
   const url = `mongodb://127.0.0.1/${databaseName}`;

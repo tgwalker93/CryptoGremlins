a40b4e442af44a2cf81b793287006822f3aaaab0
Tyler Walker <tgwalker93@gmail.com>
2022-04-21 01:50:39 -0600

    trying this test

--- client/src/pages/crypto-list/crypto-list.test.js
+++ client/src/pages/crypto-list/crypto-list.test.js
@@ -8,14 +8,13 @@
  import {render} from '@testing-library/react'
  
  
- test('If Crypto List Page is rendered', () => {
-   const page  = ReactDOM.render(<CryptoListPage />, 
-     document.getElementById('root') || document.createElement('div'));
- })
+//  test('If Crypto List Page is rendered', () => {
+//    const page  = ReactDOM.render(<CryptoListPage />, 
+//      document.getElementById('root') || document.createElement('div'));
+//  })
 
-//  test('If Crypto Profile Page is rendered', () => {
-//   const page  = render(<CryptoListPage />);
-
-//     expect(page.baseElement.textContent).toEqual("View Crypto Projects");
-// });
+ test('If Crypto Profile Page is rendered', async () => {
+  const page  = render(<CryptoListPage />);
+  expect(page.baseElement.textContent).toEqual("View Crypto Projects");
+});
  

65fa03348912380e61d147fb535d72b2972aa63e
adamhaze <adha9240@colorado.edu>
2022-04-21 19:22:20 -0600

    debugging mongo connection in actions

--- dataanalyzer/test/data-analyzer.test.js
+++ dataanalyzer/test/data-analyzer.test.js
@@ -1,14 +1,14 @@
 // global.TextEncoder = require("util").TextEncoder;
 // global.TextDecoder = require("util").TextDecoder;
-// require("regenerator-runtime/runtime");
-// const analyze = require('../utils/run');
-// var Comment = require("../../db/models/comment.js");
-// var Trending = require("../../db/models/trending.js");
-// const mongoose = require('mongoose');
-// const request = require("request");
-// const findComments = require('../utils/findComments');
+require("regenerator-runtime/runtime");
+const analyze = require('../utils/run');
+var Comment = require("../../db/models/comment.js");
+var Trending = require("../../db/models/trending.js");
+const mongoose = require('mongoose');
+const request = require("request");
+const findComments = require('../utils/findComments');
 
-// const databaseName = 'test';
+const databaseName = 'test';
 
 // make new file to hold the function run() --> run.js
 // analyzer.js should only connect to db and execute run()
@@ -22,108 +22,100 @@
 
 // jest.setTimeout(3000000);
 
-// beforeAll(async () => {
-//   const url = `mongodb://127.0.0.1/${databaseName}`;
+beforeAll(async () => {
+  const url = `mongodb://127.0.0.1/${databaseName}`;
+  await mongoose.connect(url, { useNewUrlParser: true });
+})
 
-//     // Connect to the Mongo DB
-//   // if (process.env.MONGODB_URI) {
-//   //   await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true});
-//   // } else {
-//   //   await mongoose.connect(url, { useNewUrlParser: true }) // local mongo url
-//   // }
+afterAll(async () => {
+  await Comment.deleteMany({});
+  await Trending.deleteMany({});
+  // Closes the Mongoose connection
+  //await mongoose.connection.close();
+  await mongoose.connection.close();
+})
 
-//   await mongoose.connect(url, { useNewUrlParser: true });
-// })
+const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
 
-// afterAll(async () => {
-//   await Comment.deleteMany({});
-//   await Trending.deleteMany({});
-//   // Closes the Mongoose connection
-//   //await mongoose.connection.close();
-//   await mongoose.connection.close();
-// })
-
-// const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
-
-// describe('Integration test for writing and analyzing comment data', () => {
-//   it('Should save comment to database', async () => {
-//     const commentObj = {
-//       title: 'some title',
-//       text: 'some text',
-//       userWhoMadeComment: 'person a',
-//       timestamp: Date.now().toString(),
-//       coinTicker: 'BTC'
-//     }
-//     let userComment = new Comment(commentObj);
-//     await userComment.save();
+describe('Integration test for writing and analyzing comment data', () => {
+  it('Should save comment to database', async () => {
+    const commentObj = {
+      title: 'some title',
+      text: 'some text',
+      userWhoMadeComment: 'person a',
+      timestamp: Date.now().toString(),
+      coinTicker: 'BTC'
+    }
+    let userComment = new Comment(commentObj);
+    await userComment.save();
   
-//     const query = await Comment.findOne({title: 'some title'}).exec();
-//     expect(query.userWhoMadeComment).toBeTruthy();
-//     expect(query.text).toBe('some text');
+    const query = await Comment.findOne({title: 'some title'}).exec();
+    expect(query.userWhoMadeComment).toBeTruthy();
+    expect(query.text).toBe('some text');
   
-//   })
+  })
 
-//   it('Data analyzer should handle comments with improper format', async () => {
-//     const commentObj1 = {
-//       title: 'title 2',
-//       text: 17893834198,
-//       userWhoMadeComment: 'person b',
-//       timestamp: Date.now().toString(),
-//       coinTicker: 'BTC'
-//     };
-//     const commentObj2 = {
-//       title: 'title 3',
-//       text: 15,
-//       userWhoMadeComment: 'person c',
-//       timestamp: Date.now().toString(),
-//       coinTicker: 'BTC'
-//     };
-//     let userComment1 = new Comment(commentObj1);
-//     await userComment1.save();
+  it('Data analyzer should handle comments with improper format', async () => {
+    const commentObj1 = {
+      title: 'title 2',
+      text: 17893834198,
+      userWhoMadeComment: 'person b',
+      timestamp: Date.now().toString(),
+      coinTicker: 'BTC'
+    };
+    const commentObj2 = {
+      title: 'title 3',
+      text: 15,
+      userWhoMadeComment: 'person c',
+      timestamp: Date.now().toString(),
+      coinTicker: 'BTC'
+    };
+    let userComment1 = new Comment(commentObj1);
+    await userComment1.save();
 
-//     let userComment2 = new Comment(commentObj2);
-//     await userComment2.save();
+    let userComment2 = new Comment(commentObj2);
+    await userComment2.save();
 
-//     const query = await findComments();
-//     expect(query).toHaveLength(1);
-//   })
+    const query = await findComments();
+    expect(query).toHaveLength(1);
+  })
 
-//   it('Data analyzer is correctly identifying trending coins', async () => {
-//     const commentObj3 = {
-//       title: 'title 4',
-//       text: 'important text',
-//       userWhoMadeComment: 'person d',
-//       timestamp: Date.now().toString(),
-//       coinTicker: 'ETH'
-//     };
-//     let userComment3 = new Comment(commentObj3);
-//     await userComment3.save();
+  it('Data analyzer is correctly identifying trending coins', async () => {
+    const commentObj3 = {
+      title: 'title 4',
+      text: 'important text',
+      userWhoMadeComment: 'person d',
+      timestamp: Date.now().toString(),
+      coinTicker: 'ETH'
+    };
+    let userComment3 = new Comment(commentObj3);
+    await userComment3.save();
 
-//     const commentObj4 = {
-//       title: 'title 5',
-//       text: 'important text again',
-//       userWhoMadeComment: 'person e',
-//       timestamp: Date.now().toString(),
-//       coinTicker: 'BTC'
-//     };
-//     let userComment4 = new Comment(commentObj4);
-//     await userComment4.save();
+    const commentObj4 = {
+      title: 'title 5',
+      text: 'important text again',
+      userWhoMadeComment: 'person e',
+      timestamp: Date.now().toString(),
+      coinTicker: 'BTC'
+    };
+    let userComment4 = new Comment(commentObj4);
+    await userComment4.save();
 
-//     // execute data analyzer
-//     await analyze();
+    // execute data analyzer
+    await analyze();
 
-//     const query = await Trending.findOne({}).exec();
-//     expect(query.coinTicker).toBe('BTC');
-//     expect(query.numComments).toBe(2);
-//   })
-
-
+    const query = await Trending.findOne({}).exec();
+    expect(query.coinTicker).toBe('BTC');
+    expect(query.numComments).toBe(2);
+  })
 
 
-// });
+
+
+});
 
 
 //Above test is not working, so this is just a dummy test. The above test works on local, but not in Github Actions
-test('adds 1 + 2 to equal 3', () => {
-    expect(1+2).toBe(3);
-  });
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(1+2).toBe(3);
+//   });

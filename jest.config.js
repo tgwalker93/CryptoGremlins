4529bde9b65fd700a18e7c80379e2398970ec84b
Adam Hayes <adha9240@colorado.edu>
2022-04-26 14:52:27 -0600

    setting custom jest env

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
       "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": '<rootDir>/config/styleMock.js',
       '\\.(css|less)$': '<rootDir>/config/styleMock.js',
     },
-    testEnvironment: "node"
+    testEnvironment: "../test_env/custom-test-env.js"
   };
